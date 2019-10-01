78634a682b5f6836e88bf4ad61fefe14ce4c8ea5
GekkieFoxxie <55511492+GekkieFoxxie@users.noreply.github.com>
2019-10-01 10:01:19 +0200

    Dobbelsteen JS Update

--- js/dobbelsteen.js
+++ js/dobbelsteen.js
@@ -5,10 +5,61 @@ var uitvoerDobbelsteen = document.getElementById("uitvoerDobbelsteen");
 btnDobbelsteen.addEventListener("click", function() {
 	
 	let getal = getRandomInt(1,6);
+	let image = document.createElement("img");
 	
-	uitvoerDobbelsteen.innerHTML = getal;
+	
+	/*if(getal === 1) {
+		image.src = "../Images/Dobbelsteen1.gif"		
+		
+	}
+	else if(getal === 2) {
+		image.src = "../Images/Dobbelsteen2.gif"
+	}	
+	else if(getal === 3) {
+		image.src = "../Images/Dobbelsteen3.gif"
+	}	
+	else if(getal === 4) {
+		image.src = "../Images/Dobbelsteen4.gif"
+	}	
+	else if(getal === 5) {
+		image.src = "../Images/Dobbelsteen5.gif"
+	}	
+	else if(getal === 6) {
+		image.src = "../Images/Dobbelsteen6.gif"
+	}*/
+	switch(getal) {
+		case 1:
+			image.src="../Images/Dobbelsteen1.gif"
+			break;
+			
+		case 2:
+			image.src="../Images/Dobbelsteen2.gif"
+			break;
+			
+		case 3:
+			image.src="../Images/Dobbelsteen3.gif"
+			break;
+			
+		case 4:
+			image.src="../Images/Dobbelsteen4.gif"
+			break;
+			
+		case 5:
+			image.src="../Images/Dobbelsteen5.gif"
+			break;
+			
+		case 6:
+			image.src="../Images/Dobbelsteen6.gif"
+			break;
+			
+	}
+	uitvoerDobbelsteen.innerHTML = "";
+	
+	uitvoerDobbelsteen.append(image);
 });
 
+
+//functie om een willekeurig getal te verkrijgen tussen min en max. Min en max zijn parameters die worden meegegeven wanneer de functie wordt aangeroepen
 function getRandomInt(min, max) {
 	return Math.round(Math.random() * (max - min) + min);
 }
